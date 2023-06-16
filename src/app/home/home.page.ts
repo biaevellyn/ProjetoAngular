58c3132f4e76ed3d6519bbc5f8f5013d61e634e0
biaevellyn <125669410+biaevellyn@users.noreply.github.com>
2023-06-16 15:53:05 +0000

    calculadora

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
@@ -6,8 +7,46 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
 })
 export class HomePage {
-  constructor() {}
+  valor1:any = null;
+  valor2:any = null;
+  operador:any = null;
+  resultado:any = null;
+
+  SalvarValor(numero : any){
+    if(this.valor1==null){
+      this.valor1=numero;
+    }
+    else{
+      if(this.valor2==null){
+        this.valor2=numero;
+      } 
+    } 
+      console.log(this.valor1);
+      console.log(this.valor2);
+      
+    }
+      operacao(operador:any){
+    this.operador=operador
+  }
+  calcular() {
+    if(this.operador== '+'){
+      this.resultado=this.valor1+this.valor2;
+    } else if(this.operador== '-'){
+      this.resultado=this.valor1 - this.valor2;
+    } else if(this.operador== '*'){
+      this.resultado=this.valor1 * this.valor2;
+    } else if(this.operador== '/'){
+      this.resultado=this.valor1 / this.valor2;
+    }
+    console.log(this.operador);
+  }
+
+  limpar() {
+    
+  }
 }
+
+
